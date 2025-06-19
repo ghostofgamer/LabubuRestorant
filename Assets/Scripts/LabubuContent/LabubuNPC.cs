20c24fca4133cba049df87f4e47a98b5f626dae2
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-06-19 15:59:48 +0400

    AddNPCTrafficMoved

--- Assets/Scripts/LabubuContent/LabubuNPC.cs
+++ Assets/Scripts/LabubuContent/LabubuNPC.cs
@@ -1,4 +1,4 @@
-using System;
+using TrafficCity;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,42 +8,57 @@ namespace LabubuContent
     {
         [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private Animator _animator;
-        [SerializeField] private GameObject _path;
-
+        [SerializeField] private Renderer _labubuRenderer;
+        
+        private NPCTraffic _npcTraffic;
         private Transform[] _points;
         private int _index = 0;
         private float _minDistance = 1f;
-
-        private void Start()
-        {
-            _points = new Transform[_path.transform.childCount];
-
-            for (int i = 0; i < _points.Length; i++)
-                _points[i] = _path.transform.GetChild(i);
-        }
 
         private void Update()
         {
             Roam();
         }
 
-        private void Roam()
+        public void Init(GameObject path,NPCTraffic npcTraffic,Texture texture)
+        {
+            _npcTraffic = npcTraffic;
+            
+            Material newMaterial = new Material(_labubuRenderer.sharedMaterial);
+            newMaterial.mainTexture = texture;
+            _labubuRenderer.material = newMaterial;
+            
+            _points = new Transform[path.transform.childCount];
+
+            for (int i = 0; i < _points.Length; i++)
+                _points[i] = path.transform.GetChild(i);
+        } 
+
+        /*private void Roam()
         {
             if (Vector3.Distance(transform.position, _points[_index].position) < _minDistance)
                 _index = (_index + 1) % _points.Length; 
 
             _navMeshAgent.SetDestination(_points[_index].position);
             _animator.SetFloat("Vertical", !_navMeshAgent.isStopped ? 1 : 0);
-            
-            
-            /*if (Vector3.Distance(transform.position, _points[_index].position) < _minDistance)
-                if (_index >= 0 && _index < _points.Length)
-                    _index += 1;
-                else
-                    _index = 0;
+        }*/
+        
+        private void Roam()
+        {
+            if (Vector3.Distance(transform.position, _points[_index].position) < _minDistance)
+            {
+                _index = (_index + 1) % _points.Length;
+
+                if (_index == 0)
+                {
+                    _npcTraffic.DecreaseActiveNPC();
+                    gameObject.SetActive(false);
+                    return;
+                }
+            }
 
             _navMeshAgent.SetDestination(_points[_index].position);
-            _animator.SetFloat("Vertical", !_navMeshAgent.isStopped ? 1 : 0);*/
+            _animator.SetFloat("Vertical", !_navMeshAgent.isStopped ? 1 : 0);
         }
     }
 }
---
+++ Assets/Scripts/TrafficCity/NPCTraffic.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using LabubuContent;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TrafficCity
+{
+    public class NPCTraffic : MonoBehaviour
+    {
+        [SerializeField] private List<SpawnPoint> _spawnPoints = new List<SpawnPoint>();
+        [SerializeField] private LabubuNPC[] _prefabs;
+        [SerializeField] private Transform _container;
+        [SerializeField] private int _spawnAmount;
+        [SerializeField] private Texture[] _textures;
+
+        private List<ObjectPool<LabubuNPC>> _labubuNPCPools = new List<ObjectPool<LabubuNPC>>();
+        private int _activeNPC = 0;
+        private WaitForSeconds _waitOneSecond = new WaitForSeconds(1f);
+        private WaitForSeconds _waitFiveSeconds = new WaitForSeconds(35f);
+
+        private void Start()
+        {
+            foreach (var labubuNpcPrefab in _prefabs)
+            {
+                var clientPool = new ObjectPool<LabubuNPC>(labubuNpcPrefab, _spawnAmount, _container);
+                clientPool.EnableAutoExpand();
+                _labubuNPCPools.Add(clientPool);
+            }
+
+            StartCoroutine(SpawnNPC());
+        }
+
+        private IEnumerator SpawnNPC()
+        {
+            while (true)
+            {
+                if (_activeNPC > 10)
+                    yield return _waitFiveSeconds;
+                else
+                    yield return _waitOneSecond;
+
+                int index = Random.Range(0, _spawnPoints.Count);
+                SpawnRandomClient(_spawnPoints[index]);
+            }
+        }
+
+        public void SpawnRandomClient(SpawnPoint spawnPoint)
+        {
+            ObjectPool<LabubuNPC> randomPool = _labubuNPCPools[Random.Range(0, _labubuNPCPools.Count)];
+            LabubuNPC labubuNPC = randomPool.GetFirstObject();
+
+            if (labubuNPC == null)
+            {
+                /*labubuNPC = Instantiate(_prefabs[0], _container);
+                SetPosition(labubuNPC, spawnPoint);*/
+            }
+            else
+            {
+                SetPosition(labubuNPC, spawnPoint);
+                labubuNPC.gameObject.SetActive(true);
+            }
+        }
+
+        private void SetPosition(LabubuNPC labubuNpc, SpawnPoint spawnPoint)
+        {
+            labubuNpc.transform.position = spawnPoint.spawnPosition.position;
+            labubuNpc.Init(spawnPoint.pathGroups[Random.Range(0, spawnPoint.pathGroups.Count)], this,
+                _textures[Random.Range(0, _textures.Length)]);
+        }
+
+        public void IncreaseActiveNPC()
+        {
+            _activeNPC++;
+        }
+
+        public void DecreaseActiveNPC()
+        {
+            _activeNPC--;
+
+            if (_activeNPC <= 0)
+                _activeNPC = 0;
+        }
+    }
+
+    [System.Serializable]
+    public class SpawnPoint
+    {
+        public Transform spawnPosition;
+        public List<GameObject> pathGroups;
+    }
+}
